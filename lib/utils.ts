d470d7569c277c5767e3fe144e0ed3b690c05689
thecodesalim <samatazu@gmail.com>
2025-08-10 14:18:20 +0100

    fix: date format

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,13 +14,13 @@ type FormattedDateTime = {
 export function formatDateTime(isoString: string): FormattedDateTime {
   const date = new Date(isoString);
 
-  const formattedDate = date.toLocaleDateString("en-US", {
-    month: "2-digit",
+  const formattedDate = date.toLocaleDateString("en-GB", {
     day: "2-digit",
+    month: "2-digit",
     year: "numeric",
   });
 
-  const formattedTime = date.toLocaleTimeString("en-US", {
+  const formattedTime = date.toLocaleTimeString("en-GB", {
     hour: "numeric",
     minute: "2-digit",
     hour12: true,
